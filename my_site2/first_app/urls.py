4b2a75aa0dd50cbf8859a81b0092358f3babd44e
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-08-29 17:12:51 +0500

    First steps with dynamic routing

--- my_site2/first_app/urls.py
+++ my_site2/first_app/urls.py
@@ -1,6 +1,13 @@
+from xml.etree.ElementInclude import include
 from django.urls import path
 from . import views
 
+# urlpatterns = [
+#   path('simple_view', views.simple_view),
+#   path('sports_view', views.sports_page),
+#   path('finance_view', views.finance_news)
+# ]
+
 urlpatterns = [
-  path('', views.simple_view)
+  path('<topic>/', views.news_view)
 ]
--- my_site2/first_app/views.py
+++ my_site2/first_app/views.py
@@ -2,5 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def simple_view(request):
-  return HttpResponse("SIMPLE VIEW") 
+
+articles = {
+  'sports': 'Sports Page',
+  'finance': 'Finance Page',
+  'politics': 'Politics Page'
+}
+
+# def sports_page(request):
+#   return HttpResponse(articles['sports'])
+
+# def finance_news(request):
+#   return HttpResponse(articles['finance'])
+
+# def simple_view(request):
+#   return HttpResponse("SIMPLE VIEW") 
+
+def news_view(request, topic):
+  return HttpResponse(articles[topic])
