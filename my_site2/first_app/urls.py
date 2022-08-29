ea8c5fe1de7870d6ee464c947543afa6aac5aa46
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-08-29 19:42:56 +0500

    Connecting an HTML file

--- my_site2/first_app/urls.py
+++ my_site2/first_app/urls.py
@@ -8,8 +8,15 @@ from . import views
 #   path('finance_view', views.finance_news)
 # ]
 
+# CONNECTING TEMPLATES
 urlpatterns = [
-  path('<topic>/', views.news_view, name='topic-page'),
-  path('<int:num1>/<int:num2>', views.add_view),
-  path('<int:num_page>', views.num_page_view)
+  path('', views.simple_view) #domain.com/first_app
 ]
+
+
+####################################################
+# urlpatterns = [
+#   path('<topic>/', views.news_view, name='topic-page'),
+#   path('<int:num1>/<int:num2>', views.add_view),
+#   path('<int:num_page>', views.num_page_view)
+# ]
--- my_site2/first_app/views.py
+++ my_site2/first_app/views.py
@@ -8,11 +8,16 @@ from django.urls import reverse
 
 # Create your views here.
 
-articles = {
-  'sports': 'Sports Page',
-  'finance': 'Finance Page',
-  'politics': 'Politics Page'
-}
+# CONNECTING TEMPLATES
+def simple_view(request):
+  return render(request, 'first_app/example.html') #render(request, 'templatename')
+
+#############################################################
+# articles = {
+#   'sports': 'Sports Page',
+#   'finance': 'Finance Page',
+#   'politics': 'Politics Page'
+# }
 
 # def sports_page(request):
 #   return HttpResponse(articles['sports'])
@@ -26,27 +31,27 @@ articles = {
 # def news_view(request, topic):
 #   return HttpResponse(articles[topic])
 
-def news_view(request, topic):
-  try:
-    result = articles[topic]
-    return HttpResponse(result)
-  except:
-    result = f'no page found for {topic} topic'
-    raise Http404(result) # --> later on we can add 404 html template
+# def news_view(request, topic):
+#   try:
+#     result = articles[topic]
+#     return HttpResponse(result)
+#   except:
+#     result = f'no page found for {topic} topic'
+#     raise Http404(result) # --> later on we can add 404 html template
 
 # domain.com/first_app/0 ---> domain.com/first_app/finance
 
-def num_page_view(request, num_page):
-  topics_list = list(articles.keys())
-  topic = topics_list[num_page]
-  #result = articles[topic]
+# def num_page_view(request, num_page):
+#   topics_list = list(articles.keys())
+#   topic = topics_list[num_page]
+#   #result = articles[topic]
 
-  webpage = reverse('topic-page', args=[topic]) #reverse(name, args[])
-  return HttpResponseRedirect(webpage)
+#   webpage = reverse('topic-page', args=[topic]) #reverse(name, args[])
+#   return HttpResponseRedirect(webpage)
   
 
 
-def add_view(request, num1, num2):
-  add_result = num1 + num2
-  result = f'{num1} + {num2} = {add_result}'
-  return HttpResponse(str(result))
+# def add_view(request, num1, num2):
+#   add_result = num1 + num2
+#   result = f'{num1} + {num2} = {add_result}'
+#   return HttpResponse(str(result))
