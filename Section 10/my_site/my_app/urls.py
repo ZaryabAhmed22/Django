9031f1863f6facf51caa7384ecb620ed076203ca
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-08-30 17:53:12 +0500

    Using the URL tag in buttons

--- Section 10/my_site/my_app/urls.py
+++ Section 10/my_site/my_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+# register the app namespace
+# used in URL names
+app_name = 'my_app'
+
 urlpatterns = [
-  path('variable/', views.variable_view),
-  path('example/', views.example_view)
+  path('variable/', views.variable_view, name='variable'),
+  path('example/', views.example_view, name='example')
   ]
