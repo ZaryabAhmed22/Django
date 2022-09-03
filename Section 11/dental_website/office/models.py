535ccc99e387ecc50aa734e65133d9cd403d7777
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-09-03 22:37:15 +0500

    Showing model data on html file

--- Section 11/dental_website/office/models.py
+++ Section 11/dental_website/office/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Patient(models.Model):
@@ -6,7 +7,8 @@ class Patient(models.Model):
   # Check model field reference in the django documentation
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
-  age = models.IntegerField()
+  age = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(120)])
+  heartrate = models.IntegerField(default=60, validators=[MinValueValidator(30), MaxValueValidator(300)]) #null=True
 
   def __str__(self):
     return f'{self.first_name}, {self.last_name}, {self.age}'
