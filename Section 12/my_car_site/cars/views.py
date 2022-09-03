db970a6c48065706cc08b794c32f266b734dbe1e
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-09-04 00:00:57 +0500

    Adding cars to the Car model using POST request

--- Section 12/my_car_site/cars/views.py
+++ Section 12/my_car_site/cars/views.py
@@ -1,5 +1,6 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from . import models
 
 # Create your views here.
@@ -9,7 +10,14 @@ def list(request):
   return render(request,'cars/list.html', context=context)
 
 def add(request):
-  return render(request, 'cars/add.html') 
+  print(request.POST)
+  if(request.method =='POST'):
+    brand = request.POST['brand']
+    year = int(request.POST['year'])
+    models.Car.objects.create(brand=brand, year=year)
+    return redirect(reverse('cars:list'))
+  else:
+    return render(request, 'cars/add.html') 
 
 def delete(request):
   return render(request, 'cars/delete.html')
