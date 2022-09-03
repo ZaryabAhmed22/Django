9f41c4b4d0b82a7c45fad1b63543688ca4499e83
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-09-03 23:17:47 +0500

    created Car model and made migrations

--- Section 12/my_car_site/cars/models.py
+++ Section 12/my_car_site/cars/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+  # pk 
+  brand = models.CharField(max_length=30)
+  year = models.IntegerField()
+
+  def __str__(self):
+    return f'Car is {self.brand}, model: {self.year}'
+
+# after that --> python manage.py makemigrations cars
