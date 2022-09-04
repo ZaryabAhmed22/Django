d87feda24ce95203e074841d15f7abd7a799b849
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-09-04 19:40:19 +0500

    fields and fieldsets

--- Section 12/my_car_site/cars/admin.py
+++ Section 12/my_car_site/cars/admin.py
@@ -3,5 +3,13 @@ from cars.models import Car
 
 # Register your models here.
 
-admin.site.register(Car)
+# admin.site.register(Car)
 
+#ModelNameAdmin
+class CarAdmin(admin.ModelAdmin):
+  #fields = ['year', 'brand'] #order of fields
+
+  #field sets/ field segmenting
+  fieldsets= [('TIME INFORMATION', {'fields': ['year']}), ('CAR INFORMATION', {'fields': ['brand']})]
+
+admin.site.register(Car, CarAdmin)
