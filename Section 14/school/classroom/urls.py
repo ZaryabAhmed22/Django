f1614269c447b575444b4d8fdd2824de05bd50be
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-09-05 23:49:59 +0500

    Creating first CBV and passing a url for it

--- Section 14/school/classroom/urls.py
+++ Section 14/school/classroom/urls.py
@@ -3,5 +3,7 @@ from . import views
 app_name = 'classroom'
 
 urlpatterns = [
-    path('', views.home_view, name = 'home') #path expects a function
+    # path('', views.home_view, name = 'home') #path expects a function
+    path('', views.HomeView.as_view(), name='home'), # CBV
+    path('thank_you/', views.ThankYouView.as_view(), name='thank_you')
 ]
