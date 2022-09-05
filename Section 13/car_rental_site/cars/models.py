5c3c5ca298209608f5ecff173cbb6ebf1ab1694f
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-09-05 20:49:34 +0500

    Connecting a model to a from manually

--- Section 13/car_rental_site/cars/models.py
+++ Section 13/car_rental_site/cars/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class ReviewModel(models.Model):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  stars = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
