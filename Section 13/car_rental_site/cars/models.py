46dc41e289491df51b931f22970f1bc8de85b40f
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-09-05 21:32:15 +0500

    Reporting errors

--- Section 13/car_rental_site/cars/models.py
+++ Section 13/car_rental_site/cars/models.py
@@ -5,4 +5,4 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class ReviewModel(models.Model):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
-  stars = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+  stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
