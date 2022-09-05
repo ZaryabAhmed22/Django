885184ae1eae81a24be2d8938dca71da887b44a9
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-09-05 18:25:19 +0500

    Overwriting form-contol class with custom css class

--- Section 13/car_rental_site/cars/forms.py
+++ Section 13/car_rental_site/cars/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-  first_name = forms.CharField(label="First Name", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
-  last_name = forms.CharField(label="Last Name", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
-  email = forms.EmailField(label="Email", widget=forms.TextInput(attrs={'class': 'form-control'}))
-  review = forms.CharField(label='Please write your review', widget=forms.TextInput(attrs={'class': 'form-control'}))
+  first_name = forms.CharField(label="First Name", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control no-border'}))
+  last_name = forms.CharField(label="Last Name", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control no-border'}))
+  email = forms.EmailField(label="Email", widget=forms.TextInput(attrs={'class': 'form-control no-border'}))
+  review = forms.CharField(label='Please write your review', widget=forms.Textarea(attrs={'class': 'form-control no-border'}))
