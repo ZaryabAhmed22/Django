cb67e370676894f6bff57cf4c122a0ebec0e6c28
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-09-05 17:58:29 +0500

    Styling the form - styling seperate labels and inputs fields

--- Section 13/car_rental_site/cars/forms.py
+++ Section 13/car_rental_site/cars/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ReviewForm(forms.Form):
-  first_name = forms.CharField(label="First Name", max_length=30)
-  last_name = forms.CharField(label="Last Name", max_length=30)
-  email = forms.EmailField(label="Email")
-  review = forms.CharField(label='Please write your review')
+  first_name = forms.CharField(label="First Name", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+  last_name = forms.CharField(label="Last Name", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control'}))
+  email = forms.EmailField(label="Email", widget=forms.TextInput(attrs={'class': 'form-control'}))
+  review = forms.CharField(label='Please write your review', widget=forms.TextInput(attrs={'class': 'form-control'}))
