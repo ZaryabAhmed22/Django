017ef7ac20582fad66cccad35208d182a1821089
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-09-05 21:07:00 +0500

    created a ModelForm

--- Section 13/car_rental_site/cars/forms.py
+++ Section 13/car_rental_site/cars/forms.py
@@ -1,7 +1,17 @@
+from dataclasses import field
 from django import forms
+from .models import ReviewModel
+from django.forms import ModelForm
 
-class ReviewForm(forms.Form):
-  first_name = forms.CharField(label="First Name", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control no-border'}))
-  last_name = forms.CharField(label="Last Name", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control no-border'}))
-  email = forms.EmailField(label="Email", widget=forms.TextInput(attrs={'class': 'form-control no-border'}))
-  review = forms.CharField(label='Please write your review', widget=forms.Textarea(attrs={'class': 'form-control no-border'}))
+# class ReviewForm(forms.Form):
+#   first_name = forms.CharField(label="First Name", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control no-border'}))
+#   last_name = forms.CharField(label="Last Name", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control no-border'}))
+#   email = forms.EmailField(label="Email", widget=forms.TextInput(attrs={'class': 'form-control no-border'}))
+#   review = forms.CharField(label='Please write your review', widget=forms.Textarea(attrs={'class': 'form-control no-border'}))
+
+
+# Creating a model form
+class ReviewForm(ModelForm):
+  class Meta:
+    model = ReviewModel
+    fields = ['first_name', 'last_name']
