45360addd8e13defd32b5c1cb789de50e5b6c89b
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-09-06 18:29:40 +0500

    Added more fields to the Teacher Model

--- Section 14/school/classroom/models.py
+++ Section 14/school/classroom/models.py
@@ -5,6 +5,10 @@ class Teacher(models.Model):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
   subject = models.CharField(max_length=30)
+  qualification = models.CharField(default='MSC',max_length=50)
+  deparment = models.CharField(default= 'Engineering', max_length=50)
+  specialization = models.CharField(default='Engineering',max_length=50)
+  number_of_subjects = models.IntegerField(default=1)
 
   def __str__(self):
     return f'{self.first_name} {self.last_name} teaches {self.subject}'
