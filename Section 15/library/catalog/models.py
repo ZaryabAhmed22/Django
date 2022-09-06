716a70bb0d25e5ee03d4ab2ebe1af934d3e0df0f
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-09-07 01:38:33 +0500

    Author Model

--- Section 15/library/catalog/models.py
+++ Section 15/library/catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Genre(models.Model):
@@ -13,3 +14,18 @@ class Book(models.Model):
   # model.SET_NULL --> null value if no author is selected or the author is deleted
   # ForeignKey('Author') --> this model is linked to the Author model
   # MORE TO COME HERE FOR BOOK
+
+class Author(models.Model):
+  first_name = models.CharField(max_length=200)
+  last_name = models.CharField(max_length=200)
+  date_of_birth = models.DateField(null=True, blank=True)
+  date_of_death = models.DateField(null=True, blank=True)
+
+  class Meta:
+    ordering = ['last_name', 'first_name']
+
+  def get_absolute_url(self):
+      return reverse("author_detail", kwargs={"pk": self.pk})
+  
+  def __str__(self):
+    return f'{self.last_name}, {self.first_name}'
