e03ddb013e37bf5218ce31da80884c20d01164ef
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-09-06 17:55:27 +0500

    Create View

--- Section 14/school/classroom/views.py
+++ Section 14/school/classroom/views.py
@@ -1,18 +1,21 @@
 from django.urls import reverse_lazy
 from django.shortcuts import render
-from django.views.generic import TemplateView, FormView
+from django.views.generic import TemplateView, FormView, CreateView
 from classroom.froms import ContactForm
+from classroom.models import Teacher
 
 # Create your views here.
 def home_view(request):
   return render(request, 'classroom/home.html')
 
+####### TEMPLATE VIEW #######
 class HomeView(TemplateView):
   template_name = 'classroom/home.html'
 
 class ThankYouView(TemplateView):
   template_name= 'classroom/thank_you.html'
 
+####### FORM VIEW #######
 class ContactFormView(FormView):
   form_class = ContactForm
   template_name= 'classroom/contact.html'
@@ -25,3 +28,14 @@ class ContactFormView(FormView):
   def form_valid(self, form):
     print(form.cleaned_data)
     return super().form_valid(form)
+
+####### CREATE VIEW #######
+class TeacherCreateView(CreateView):
+  # Step 1: Connect the model
+  model = Teacher
+
+  # Step 2: Fields you want to display
+  fields = '__all__'
+  success_url= reverse_lazy('classroom:thank_you')
+
+  # template --> dejango by default searches for a template named model_form.html
