ca4763dc32745ba993e97077309adea23e94b2e5
ZaryabAhmed22 <zaryabzak22@gmail.com>
2022-09-07 15:25:08 +0500

    Created CBV to add and view detail

--- Section 15/library/catalog/views.py
+++ Section 15/library/catalog/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Author, Genre, Language, Book, BookInstance
+from django.views.generic import CreateView, DetailView
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
-  return HttpResponse("HELLO")
+  num_books = Book.objects.all().count()
+  num_instacnes = BookInstance.objects.all().count()
+
+  num_instacnes_avail = BookInstance.objects.filter(status__exact='a').count()
+
+  context = {
+    'num_book': num_books,
+    'num_instances': num_instacnes,
+    'num_instances_avail': num_instacnes_avail
+  }
+
+  return render(request, 'catalog/index.html', context=context)
+
+class BookCreate(CreateView): #book_form.html
+  model = Book
+  fields = '__all__'
+
+  #success_url = reverse_lazy('catalog:index')
+
+class BookDetail(DetailView): #book_detail
+  model = Book
